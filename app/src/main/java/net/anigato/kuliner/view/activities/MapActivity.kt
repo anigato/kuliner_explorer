ee06095d5f92599f4bef06860e280c39fbc95111
anigato <203040096@mail.unpas.ac.id>
2023-11-11 19:58:00 +0700

    memindahkan peta ke MapActivity dan activity_map dari MainActivity dan activity_main

--- app/src/main/java/net/anigato/kuliner/view/activities/MapActivity.kt
+++ app/src/main/java/net/anigato/kuliner/view/activities/MapActivity.kt
@@ -9,7 +9,6 @@ import android.graphics.Color
 import android.location.Geocoder
 import android.os.Build
 import android.os.Bundle
-import android.util.Log
 import android.view.View
 import android.view.WindowManager
 import android.widget.Toast
