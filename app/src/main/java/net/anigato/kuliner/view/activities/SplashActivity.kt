a7d8a4b155bfb908876a1e57be228cbe83f50bae
anigato <203040096@mail.unpas.ac.id>
2024-07-22 12:25:34 +0700

    merubah dan memperbaiki splash activity

--- app/src/main/java/net/anigato/kuliner/view/activities/SplashActivity.kt
+++ app/src/main/java/net/anigato/kuliner/view/activities/SplashActivity.kt
@@ -1,21 +1,28 @@
 package net.anigato.kuliner.view.activities
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.appcompat.app.AppCompatActivity
+import net.anigato.kuliner.R
 
 class SplashActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash) // Pastikan ada layout activity_splash
 
         // Mendapatkan kota dari intent sebelumnya
         val strCity = intent.getStringExtra("strCity")
 
-        // Langsung navigasi ke FoodsActivity
-        val intent = Intent(this, FoodsActivity::class.java)
-        intent.putExtra("strCity", strCity)
-        startActivity(intent)
-        finish()
+        // Menampilkan splash screen selama 2 detik
+        Handler(Looper.getMainLooper()).postDelayed({
+            // Navigasi ke FoodsActivity setelah penundaan
+            val intent = Intent(this, FoodsActivity::class.java)
+            intent.putExtra("strCity", strCity)
+            startActivity(intent)
+            finish()
+        }, 1000) // 2000 milidetik = 2 detik
     }
 }
